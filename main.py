809eb5fe4362b527493e911a1229b6137ce25c70
Алина Демчук <alina2805@bk.ru>
2024-06-07 15:12:33 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 a = 'Hello'
 print(len(a))
+
+#2 3 5
+print(2 + 3)
+print(5 - 2)
+numbers = (2, 3, 5)
+average = sum(numbers)/len(numbers)
+print(average)
+print("Понедельник, Вторник")
+#a, b, c, f
+#(a+b)*(a+c)
+print((2+3)*(2+5))
+print(((35**3)/2))
